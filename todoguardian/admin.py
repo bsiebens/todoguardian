5531c2035de9cdde89ee6ed7627e6c259aa8734c
Bernard Siebens <bernard@siebens.org>
2024-06-11 08:29:03 +0200

    Fleshing out some of the admin
    Signed-off-by: Bernard Siebens <bernard@siebens.org>

--- todoguardian/admin.py
+++ todoguardian/admin.py
@@ -4,4 +4,15 @@ from .models import Todo
 
 
 @admin.register(Todo)
-class TodoAdmin(admin.ModelAdmin): ...
+class TodoAdmin(admin.ModelAdmin):
+    def completed(self, instance):
+        return instance.is_completed
+
+    completed.boolean = True
+    completed.short_description = "Completed ?"
+
+    date_hierarchy = "due_date"
+    list_display = ["id", "description", "priority", "due_date", "start_date", "completed"]
+    list_display_links = ["description"]
+    list_filter = ["priority"]
+    ordering = ["-due_date", "start_date", "priority"]
