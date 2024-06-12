33bdde525962ccda07ef760ffd6cdb68a6543d60
Bernard Siebens <bernard@siebens.org>
2024-06-13 00:42:22 +0200

    Further work and updates to the frontend, trying to rebuild what we have created in the past for taskguardian

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,16 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    fontFamily: {
+      "sans": ["Ubuntu", "Noto Sans"],
+    }
   },
   plugins: [
     require("daisyui"),
+    require('@tailwindcss/typography'),
   ],
   daisyui: {
-    themes: ["bumblebee"],
+    themes: ["emerald", "night"],
   },
 }
 
